d3e034e3845ba5727a937c676dcbc65c52b4b3de
Robert Guske <rguske@vmware.com>
2023-02-08 10:18:45 +0100

    backup
    Signed-off-by: Robert Guske <rguske@vmware.com>

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # copy ZSH config files into Git dir dotfiles
-cp ~/.zshrc ~/.zsh_aliases ~/dotfiles.sh '$DOTFILESDIR'
+cp -r ~/.zshrc ~/.zsh_aliases ~/dotfiles.sh ${DOTFILESDIR}
 
-ls -rtla '$DOTFILESDIR'
+ls -rtla ${DOTFILESDIR}
 
-cd '$DOTFILESDIR'
+cd ${DOTFILESDIR}
 
 git status
 
 git add .
 
-git commit -s -m "backup $date"
+git commit -s -m "backup ${DATE}"
 
 gloga
 
-cd '$HOME'
+cd ${HOME}
